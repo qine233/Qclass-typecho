97c5e6c2e21e7d7fb97a2d746ae6b0ddaec46ffb
qine <1351856278@qq.com>
2021-10-02 18:17:16 +0800

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,249 @@
+<?php
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+
+function themeConfig($logo) {
+    $ClassOne = new Typecho_Widget_Helper_Form_Element_Text('ClassOne', NULL, NULL, _t('星期一第一节课'), _t('星期一第一节课名称'));
+    $logo->addInput($ClassOne);
+    
+
+
+    $ClassTwo = new Typecho_Widget_Helper_Form_Element_Text('ClassTwo', NULL, NULL, _t(' '), _t('教室名称'));
+    $logo->addInput($ClassTwo);
+    
+	
+    $ClassThree = new Typecho_Widget_Helper_Form_Element_Text('ClassThree', NULL, NULL, _t(''), _t('教师名称'));
+    $logo->addInput($ClassThree);
+    
+    $ClassA = new Typecho_Widget_Helper_Form_Element_Text('ClassA', NULL, NULL, _t('星期一第二节课'), _t('星期一第二节课名称'));
+    $logo->addInput($ClassA);
+    
+
+
+    $ClassB = new Typecho_Widget_Helper_Form_Element_Text('ClassB', NULL, NULL, _t(' '), _t('教室名称'));
+    $logo->addInput($ClassB);
+    
+	
+    $ClassC = new Typecho_Widget_Helper_Form_Element_Text('ClassC', NULL, NULL, _t(''), _t('教师名称'));
+    $logo->addInput($ClassC);
+
+    $ClassD = new Typecho_Widget_Helper_Form_Element_Text('ClassD', NULL, NULL, _t('星期一第三节课'), _t('星期一第三节课名称'));
+    $logo->addInput($ClassD);
+    
+
+
+    $ClassE = new Typecho_Widget_Helper_Form_Element_Text('ClassE', NULL, NULL, _t(' '), _t('教室名称'));
+    $logo->addInput($ClassE);
+    
+	
+    $ClassF = new Typecho_Widget_Helper_Form_Element_Text('ClassF', NULL, NULL, _t(''), _t('教师名称'));
+    $logo->addInput($ClassF);
+    
+    $ClassG = new Typecho_Widget_Helper_Form_Element_Text('ClassG', NULL, NULL, _t('星期一第四节课'), _t('星期一第四节课名称'));
+    $logo->addInput($ClassG);
+    
+
+
+    $ClassH = new Typecho_Widget_Helper_Form_Element_Text('ClassH', NULL, NULL, _t(' '), _t('教室名称'));
+    $logo->addInput($ClassH);
+    
+	
+    $ClassI = new Typecho_Widget_Helper_Form_Element_Text('ClassI', NULL, NULL, _t(''), _t('教师名称'));
+    $logo->addInput($ClassI);
+
+    $ClassJ = new Typecho_Widget_Helper_Form_Element_Text('ClassJ', NULL, NULL, _t('星期二第一节课'), _t('星期二第一节课名称'));
+    $logo->addInput($ClassJ);
+    
+
+
+    $ClassK = new Typecho_Widget_Helper_Form_Element_Text('ClassK', NULL, NULL, _t(' '), _t('教室名称'));
+    $logo->addInput($ClassK);
+    
+	
+    $ClassL = new Typecho_Widget_Helper_Form_Element_Text('ClassL', NULL, NULL, _t(''), _t('教师名称'));
+    $logo->addInput($ClassL);
+
+    $ClassM = new Typecho_Widget_Helper_Form_Element_Text('ClassM', NULL, NULL, _t('星期二第二节课'), _t('星期二第二节课名称'));
+    $logo->addInput($ClassM);
+    
+
+
+    $ClassN = new Typecho_Widget_Helper_Form_Element_Text('ClassN', NULL, NULL, _t(' '), _t('教室名称'));
+    $logo->addInput($ClassN);
+    
+	
+    $ClassO = new Typecho_Widget_Helper_Form_Element_Text('ClassO', NULL, NULL, _t(''), _t('教师名称'));
+    $logo->addInput($ClassO);
+
+    $ClassP = new Typecho_Widget_Helper_Form_Element_Text('ClassP', NULL, NULL, _t('星期二第三节课'), _t('星期二第三节课名称'));
+    $logo->addInput($ClassP);
+    
+
+
+    $ClassQ = new Typecho_Widget_Helper_Form_Element_Text('ClassQ', NULL, NULL, _t(' '), _t('教室名称'));
+    $logo->addInput($ClassQ);
+    
+	
+    $ClassR = new Typecho_Widget_Helper_Form_Element_Text('ClassR', NULL, NULL, _t(''), _t('教师名称'));
+    $logo->addInput($ClassR);
+
+
+
+    $ClassS = new Typecho_Widget_Helper_Form_Element_Text('ClassS', NULL, NULL, _t('星期二第四节课'), _t('星期二第四节课名称'));
+    $logo->addInput($ClassS);
+    
+
+
+    $ClassT = new Typecho_Widget_Helper_Form_Element_Text('ClassT', NULL, NULL, _t(' '), _t('教室名称'));
+    $logo->addInput($ClassT);
+    
+	
+    $ClassU = new Typecho_Widget_Helper_Form_Element_Text('ClassU', NULL, NULL, _t(''), _t('教师名称'));
+    $logo->addInput($ClassU);
+
+    $ClassV = new Typecho_Widget_Helper_Form_Element_Text('ClassV', NULL, NULL, _t('星期三第一节课'), _t('星期三第一节课名称'));
+    $logo->addInput($ClassV);
+    
+
+
+    $ClassW = new Typecho_Widget_Helper_Form_Element_Text('ClassW', NULL, NULL, _t(' '), _t('教室名称'));
+    $logo->addInput($ClassW);
+    
+	
+    $ClassX = new Typecho_Widget_Helper_Form_Element_Text('ClassX', NULL, NULL, _t(''), _t('教师名称'));
+    $logo->addInput($ClassX);
+
+    $ClassY = new Typecho_Widget_Helper_Form_Element_Text('ClassY', NULL, NULL, _t('星期三第二节课'), _t('星期三第二节课名称'));
+    $logo->addInput($ClassY);
+    
+
+
+    $ClassZ = new Typecho_Widget_Helper_Form_Element_Text('ClassZ', NULL, NULL, _t(' '), _t('教室名称'));
+    $logo->addInput($ClassZ);
+    
+	
+    $ClassAB = new Typecho_Widget_Helper_Form_Element_Text('ClassAB', NULL, NULL, _t(''), _t('教师名称'));
+    $logo->addInput($ClassAB);
+
+    $ClassAC = new Typecho_Widget_Helper_Form_Element_Text('ClassAC', NULL, NULL, _t('星期三第三节课'), _t('星期三第三节课名称'));
+    $logo->addInput($ClassAC);
+    
+
+
+    $ClassAD = new Typecho_Widget_Helper_Form_Element_Text('ClassAD', NULL, NULL, _t(' '), _t('教室名称'));
+    $logo->addInput($ClassAD);
+    
+	
+    $ClassAE = new Typecho_Widget_Helper_Form_Element_Text('ClassAE', NULL, NULL, _t(''), _t('教师名称'));
+    $logo->addInput($ClassAE);
+
+    $ClassAF = new Typecho_Widget_Helper_Form_Element_Text('ClassAF', NULL, NULL, _t('星期三第四节课'), _t('星期三第四节课名称'));
+    $logo->addInput($ClassAF);
+    
+
+
+    $ClassAG = new Typecho_Widget_Helper_Form_Element_Text('ClassAG', NULL, NULL, _t(' '), _t('教室名称'));
+    $logo->addInput($ClassAG);
+    
+	
+    $ClassAH = new Typecho_Widget_Helper_Form_Element_Text('ClassAH', NULL, NULL, _t(''), _t('教师名称'));
+    $logo->addInput($ClassAH);
+
+    $ClassAI = new Typecho_Widget_Helper_Form_Element_Text('ClassAI', NULL, NULL, _t('星期四第一节课'), _t('星期四第一节课名称'));
+    $logo->addInput($ClassAI);
+    
+
+
+    $ClassAJ = new Typecho_Widget_Helper_Form_Element_Text('ClassAJ', NULL, NULL, _t(' '), _t('教室名称'));
+    $logo->addInput($ClassAJ);
+    
+	
+    $ClassAK = new Typecho_Widget_Helper_Form_Element_Text('ClassAK', NULL, NULL, _t(''), _t('教师名称'));
+    $logo->addInput($ClassAK);
+
+    $ClassAL = new Typecho_Widget_Helper_Form_Element_Text('ClassAL', NULL, NULL, _t('星期四第二节课'), _t('星期四第二节课名称'));
+    $logo->addInput($ClassAL);
+    
+
+
+    $ClassAM = new Typecho_Widget_Helper_Form_Element_Text('ClassAM', NULL, NULL, _t(' '), _t('教室名称'));
+    $logo->addInput($ClassAM);
+    
+	
+    $ClassAN = new Typecho_Widget_Helper_Form_Element_Text('ClassAN', NULL, NULL, _t(''), _t('教师名称'));
+    $logo->addInput($ClassAN);
+
+    $ClassAO = new Typecho_Widget_Helper_Form_Element_Text('ClassAO', NULL, NULL, _t('星期四第三节课'), _t('星期四第三节课名称'));
+    $logo->addInput($ClassAO);
+    
+
+
+    $ClassAP = new Typecho_Widget_Helper_Form_Element_Text('ClassAP', NULL, NULL, _t(' '), _t('教室名称'));
+    $logo->addInput($ClassAP);
+    
+	
+    $ClassAQ = new Typecho_Widget_Helper_Form_Element_Text('ClassAQ', NULL, NULL, _t(''), _t('教师名称'));
+    $logo->addInput($ClassAQ);
+
+    $ClassAR = new Typecho_Widget_Helper_Form_Element_Text('ClassAR', NULL, NULL, _t('星期四第四节课'), _t('星期四第四节课名称'));
+    $logo->addInput($ClassAR);
+    
+
+
+    $ClassAS = new Typecho_Widget_Helper_Form_Element_Text('ClassAS', NULL, NULL, _t(' '), _t('教室名称'));
+    $logo->addInput($ClassAS);
+    
+	
+    $ClassAT = new Typecho_Widget_Helper_Form_Element_Text('ClassAT', NULL, NULL, _t(''), _t('教师名称'));
+    $logo->addInput($ClassAT);
+
+    $ClassAU = new Typecho_Widget_Helper_Form_Element_Text('ClassAU', NULL, NULL, _t('星期五第一节课'), _t('星期五第一节课名称'));
+    $logo->addInput($ClassAU);
+    
+
+
+    $ClassAV = new Typecho_Widget_Helper_Form_Element_Text('ClassAV', NULL, NULL, _t(' '), _t('教室名称'));
+    $logo->addInput($ClassAV);
+    
+	
+    $ClassAW = new Typecho_Widget_Helper_Form_Element_Text('ClassAW', NULL, NULL, _t(''), _t('教师名称'));
+    $logo->addInput($ClassAW);
+
+    $ClassAX = new Typecho_Widget_Helper_Form_Element_Text('ClassAX', NULL, NULL, _t('星期五第二节课'), _t('星期五第二节课名称'));
+    $logo->addInput($ClassAX);
+    
+
+
+    $ClassAY = new Typecho_Widget_Helper_Form_Element_Text('ClassAY', NULL, NULL, _t(' '), _t('教室名称'));
+    $logo->addInput($ClassAY);
+    
+	
+    $ClassAZ = new Typecho_Widget_Helper_Form_Element_Text('ClassAZ', NULL, NULL, _t(''), _t('教师名称'));
+    $logo->addInput($ClassAZ);
+
+    $ClassAXA = new Typecho_Widget_Helper_Form_Element_Text('ClassAXA', NULL, NULL, _t('星期五第三节课'), _t('星期五第三节课名称'));
+    $logo->addInput($ClassAXA);
+    
+
+
+    $ClassAYA = new Typecho_Widget_Helper_Form_Element_Text('ClassAYA', NULL, NULL, _t(' '), _t('教室名称'));
+    $logo->addInput($ClassAYA);
+    
+	
+    $ClassAZA = new Typecho_Widget_Helper_Form_Element_Text('ClassAZA', NULL, NULL, _t(''), _t('教师名称'));
+    $logo->addInput($ClassAZA);
+
+    	
+ 
+
+    $ClassAXAA = new Typecho_Widget_Helper_Form_Element_Text('ClassAXAA', NULL, NULL, _t('星期五第四节课'), _t('星期五第四节课名称'));
+    $logo->addInput($ClassAXAA);
+    
+
+
+    $ClassAYAA = new Typecho_Widget_Helper_Form_Element_Text('ClassAYAA', NULL, NULL, _t(' '), _t('教室名称'));
+    $logo->addInput($ClassAYAA);
+    
+	
+    $ClassAZAA = new Typecho_Widget_Helper_Form_Element_Text('ClassAZAA', NULL, NULL, _t(''), _t('教师名称'));
+    $logo->addInput($ClassAZAA);
+}
---
+++ index.php
@@ -0,0 +1,153 @@
+<?php
+/**
+ * “ 这个主题仅用来展示课程表 ”
+ * @package 课程表主题
+ * @author QINE
+ * @link https://www.idkzr.com/
+ */
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title><?php $this->options->title(); ?></title>
+		<link rel="stylesheet" type="text/css" href="<?php $this->options->themeUrl('css/style.css'); ?>"/>
+	</head>
+	<body>
+		<div class="container">
+			<div class="title-one">
+				
+			</div>
+			<div class="box-flex-all">
+				<div class="box-flex-wid">
+					<ul class="date-m">
+						<li style=" font-size: 1.5rem; ">时间\星期</li>
+						<li>一</li>
+						<li>二</li>
+						<li>三</li>
+						<li>四</li>
+						<li>五</li>
+					</ul>
+				</div>
+				<div class="box-flex-f">
+					<h3>9:00-10:00</h3>
+				</div>
+				<div class="box-flex-1">
+					<h2><?php $this->options->ClassOne(); ?></h2>
+					<h4><?php $this->options->ClassTwo(); ?></h4>
+					<h5><?php $this->options->ClassThree(); ?></h5>
+				</div>
+				<div class="box-flex-1">
+				<h2><?php $this->options->ClassJ(); ?></h2>
+					<h4><?php $this->options->ClassK(); ?></h4>
+					<h5><?php $this->options->ClassL(); ?></h5>
+				</div>
+				<div class="box-flex-1">
+				<h2><?php $this->options->ClassV(); ?></h2>
+					<h4><?php $this->options->ClassW(); ?></h4>
+					<h5><?php $this->options->ClassX(); ?></h5>
+				</div>
+				<div class="box-flex-1">
+				<h2><?php $this->options->ClassAI(); ?></h2>
+					<h4><?php $this->options->ClassAJ(); ?></h4>
+					<h5><?php $this->options->ClassAK(); ?></h5>
+				</div>
+				<div class="box-flex-1">
+				<h2><?php $this->options->ClassAU(); ?></h2>
+					<h4><?php $this->options->ClassAV(); ?></h4>
+					<h5><?php $this->options->ClassAW(); ?></h5>
+				</div>
+				<div class="box-flex-f">
+					<h3>10:45-12:15</h3>
+				</div>
+				<div class="box-flex-1">
+				<h2><?php $this->options->ClassA(); ?></h2>
+					<h4><?php $this->options->ClassB(); ?></h4>
+					<h5><?php $this->options->ClassC(); ?></h5>
+				</div>
+				<div class="box-flex-1">
+				<h2><?php $this->options->ClassM(); ?></h2>
+					<h4><?php $this->options->ClassN(); ?></h4>
+					<h5><?php $this->options->ClassO(); ?></h5>
+				</div>
+				<div class="box-flex-1">
+				<h2><?php $this->options->ClassY(); ?></h2>
+					<h4><?php $this->options->ClassZ(); ?></h4>
+					<h5><?php $this->options->ClassAB(); ?></h5>
+				</div>
+				<div class="box-flex-1">
+				<h2><?php $this->options->ClassAL(); ?></h2>
+					<h4><?php $this->options->ClassAM(); ?></h4>
+					<h5><?php $this->options->ClassAN(); ?></h5>
+				</div>
+				<div class="box-flex-1">
+				<h2><?php $this->options->ClassAX(); ?></h2>
+					<h4><?php $this->options->ClassAY(); ?></h4>
+					<h5><?php $this->options->ClassAZ(); ?></h5>
+				</div>
+				<div class="box-fen">
+					
+				</div>
+				<div class="box-flex-f">
+					<h3>14:00-15:30</h3>
+				</div>
+				
+				<div class="box-flex-1">
+				<h2><?php $this->options->ClassD(); ?></h2>
+					<h4><?php $this->options->ClassE(); ?></h4>
+					<h5><?php $this->options->ClassF(); ?></h5>
+				</div>
+				<div class="box-flex-1">
+				<h2><?php $this->options->ClassP(); ?></h2>
+					<h4><?php $this->options->ClassQ(); ?></h4>
+					<h5><?php $this->options->ClassR(); ?></h5>
+				</div>
+				<div class="box-flex-1">
+				<h2><?php $this->options->ClassAC(); ?></h2>
+					<h4><?php $this->options->ClassAD(); ?></h4>
+					<h5><?php $this->options->ClassAE(); ?></h5>
+				</div>
+				<div class="box-flex-1">
+				<h2><?php $this->options->ClassAO(); ?></h2>
+					<h4><?php $this->options->ClassAP(); ?></h4>
+					<h5><?php $this->options->ClassAQ(); ?></h5>
+				</div>
+				<div class="box-flex-1">
+				<h2><?php $this->options->ClassAXA(); ?></h2>
+					<h4><?php $this->options->ClassAYA(); ?></h4>
+					<h5><?php $this->options->ClassAZA(); ?></h5>
+				</div>
+				<div class="box-flex-f">
+					<h3>15:45-17:15</h3>
+				</div>
+				<div class="box-flex-1">
+				<h2><?php $this->options->ClassG(); ?></h2>
+					<h4><?php $this->options->ClassH(); ?></h4>
+					<h5><?php $this->options->ClassI(); ?></h5>
+				</div>
+				<div class="box-flex-1">
+				<h2><?php $this->options->ClassS(); ?></h2>
+					<h4><?php $this->options->ClassT(); ?></h4>
+					<h5><?php $this->options->ClassU(); ?></h5>
+				</div>
+				<div class="box-flex-1">
+				<h2><?php $this->options->ClassAF(); ?></h2>
+					<h4><?php $this->options->ClassAG(); ?></h4>
+					<h5><?php $this->options->ClassAH(); ?></h5>
+				</div>
+				<div class="box-flex-1">
+				<h2><?php $this->options->ClassAR(); ?></h2>
+					<h4><?php $this->options->ClassAS(); ?></h4>
+					<h5><?php $this->options->ClassAT(); ?></h5>
+				
+				</div>
+				<div class="box-flex-1">
+				<h2><?php $this->options->ClassAXAA(); ?></h2>
+					<h4><?php $this->options->ClassAYAA(); ?></h4>
+					<h5><?php $this->options->ClassAZAA(); ?></h5>
+				</div>
+				
+			</div>
+		</div>
+	</body>
+</html>
